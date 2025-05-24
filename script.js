82d1fc6f5b42cab6bf32d36d6496895cca1ace22
sanjeevannp <sanj8319291@gmail.com>
2025-05-24 14:07:47 +0530

    21.54

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 let franeX = 0;
-let franeY = 1;
+let franeY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    //ctx.fillRect(100,50,100,100);
-    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    
+
     ctx.drawImage(playerImage, franeX * spriteWidth, franeY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if (franeX < 6) franeX++;
-    else franeX = 0;
+
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
