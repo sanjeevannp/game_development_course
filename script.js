aea4f5d9b1c67aaf611a8b4e5e8e164319c2ebd1
sanjeevannp <sanj8319291@gmail.com>
2025-05-25 19:40:59 +0530

    33.23

--- script.js
+++ script.js
@@ -8,24 +8,33 @@ playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 let franeX = 0;
-let franeY = 5;
-let gameFrame =0;
+let franeY = 4;
+let gameFrame = 0;
 const staggerFrames = 5;
-const spriteAnimations = [];
+const spritAnimations = [];
 const animationStates = [
-     {
+    {
         name: 'idle',
         frames: 7,
-     }
-     {
+    },
+    {
         name: 'jump',
         frames: 7,
-     }
+    }
 ]
+animationStates.forEach((state , index) => { 
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j <state.frames; s++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+    }
+});
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % 4;
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
     ctx.drawImage(playerImage, franeX, franeY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
     gameFrame++;
