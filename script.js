6c04e3beb9f928b123dc20911ce369d43c9fe0d3
sanjeevannp <sanj8319291@gmail.com>
2025-05-26 17:18:19 +0530

    44.24

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState = 'idle';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change',function(e){
+    playerState = e.target.value;
+})
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 // get Context 2d means we are getting the 2d rendering context of the canvas
@@ -25,6 +31,38 @@ const animationStates=[
     {
         name:"jump",
         frames:7
+    },
+    {
+        name:"fall",
+        frames:9
+    },
+    {
+        name:"run",
+        frames:9
+    },
+    {
+        name:"dizzy",
+        frames:11
+    },
+    {
+        name:"sit",
+        frames:5
+    },
+    {
+        name:"roll",
+        frames:7
+    },
+    {
+        name:"bite",
+        frames:7
+    },
+    {
+        name:"ko",
+        frames:12
+    },
+    {
+        name:"getHit",
+        frames:4
     }
 ];
 
@@ -45,11 +83,12 @@ console.log(spriteAnimations);
 // 5230 - is full sheet height, 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let position=Math.floor(gameFrame/staggerFrames)%6;
+    let position=Math.floor(gameFrame/staggerFrames)%spriteAnimations[playerState].loc.length;
 
-    frameX = spriteWidth * position;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
 
-    ctx.drawImage(playerImage,frameX,frameY*spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+    ctx.drawImage(playerImage,frameX,frameY,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
    
     gameFrame++;
     requestAnimationFrame(animate);
