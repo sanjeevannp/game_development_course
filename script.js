a38533a569b85eddd0a4e76a3a11902f4639e639
sanjeevannp <sanj8319291@gmail.com>
2025-05-26 12:45:56 +0530

    36.00

--- script.js
+++ script.js
@@ -29,8 +29,11 @@ animationStates.forEach((state , index) => {
     for (let j = 0; j <state.frames; s++){
         let positionX = j * spriteWidth;
         let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
     }
+    spriteAnimations[state.name] = frames;
 });
+console.log(animationStates);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
