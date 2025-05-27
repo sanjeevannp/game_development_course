89b2b00a082f871996e618b5a528568b159dca7d
sanjeevannp <sanj8319291@gmail.com>
2025-05-27 20:18:04 +0530

    1.07.05

--- project  2/script.js
+++ project  2/script.js
@@ -15,17 +15,30 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-let x = 0;
-let x2 = 2400;
+class layer {
+    constructor(Image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = this.image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x - this.speed;
+        }
+    }
+    draw(){
+
+    }
+}
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400 + x2 - gameSpeed;
-    else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
     requestAnimationFrame(animate);
 };
 animate();
