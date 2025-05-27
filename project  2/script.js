a2512477d48c20ca1e135d90fcc17edc73c9085b
sanjeevannp <sanj8319291@gmail.com>
2025-05-27 19:06:07 +0530

    1.00.48

--- project  2/script.js
+++ project  2/script.js
@@ -22,8 +22,10 @@ function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(backgroundLayer4, x, 0);
     ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400;
+    if (x < -2400) x = 2400 + x2 - gameSpeed;
     else x -= gameSpeed;
+    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+    else x2 -= gameSpeed;
     requestAnimationFrame(animate);
 };
 animate();
