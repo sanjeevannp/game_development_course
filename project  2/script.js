03dc1e966f8f2ed99ee47afa82525e4551ce8772
sanjeevannp <sanj8319291@gmail.com>
2025-05-27 18:29:02 +0530

    56.05

--- project  2/script.js
+++ project  2/script.js
@@ -2,7 +2,7 @@ const canvas =document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.Height = 700;
-let gameSpeed = 15;
+let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -16,11 +16,13 @@ const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
 let x = 0;
+let x2 = 2400;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer5, x, 0);
-    if (x < -1000) x = 0;
+    ctx.drawImage(backgroundLayer4, x, 0);
+    ctx.drawImage(backgroundLayer4, x2, 0);
+    if (x < -2400) x = 2400;
     else x -= gameSpeed;
     requestAnimationFrame(animate);
 };
