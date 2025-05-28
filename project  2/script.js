e3629e2d41e632698231b1260a5082af3460940a
sanjeevannp <sanj8319291@gmail.com>
2025-05-28 19:03:46 +0530

    1.13.00

--- project  2/script.js
+++ project  2/script.js
@@ -43,12 +43,20 @@ class layer {
     }
 }
 
+const layer1 = new layer(backgroundLayer1, 0.5);
+const layer2 = new layer(backgroundLayer2, 0.5);
+const layer3 = new layer(backgroundLayer3, 0.5);
 const layer4 = new layer(backgroundLayer4, 0.5);
+const layer5 = new layer(backgroundLayer5, 0.5);
+
+const gameObjects = [layer1, layer2 ,layer3 ,layer4 ,layer5];
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    layer4.update();
-    layer4.draw();
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate);
 };
 animate();
