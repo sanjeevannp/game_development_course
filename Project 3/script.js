73aa1a21a90340e19cb90a3105ad3efaf25767bc
sanjeevannp <sanj8319291@gmail.com>
2025-05-30 19:11:51 +0530

    1.37.15

--- Project 3/script.js
+++ Project 3/script.js
@@ -3,19 +3,32 @@ const canvas =document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100;
 
-enemy1 = {
-    x: 10,
-    y: 50,
-    width: 100,
-    height: 100,
+class Enemy {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.width = 100;
+        this.height = 100;
+    }
+    update(){
+        this.x++;
+        this.y++;
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+//const enemy1 = new Enemy();
+for (let i = 0; i < numberOfEnemies; i++){
+
 }
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    enemy1.x++;
-    enemy1.y++;
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    enemy1.update();
+    enemy1.draw();
     requestAnimationFrame(animate);
 }
 animate();
